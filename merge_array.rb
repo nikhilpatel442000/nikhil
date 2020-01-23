9251eece98b76286cec868de67b5db5b9537bf80
nikhil <nikpatel@bestpeers.com>
2020-01-23 17:52:22 +0530

    merge_array.rb

--- merge_array.rb
+++ merge_array.rb
@@ -1,19 +1,21 @@
-  class MergeArray
-      
-    def merge(array1,array2)
-        array3=[]
-        array1.delete("")
-        array2.delete("")
-        array1.length<array2.length ? maxlength=array1.length : maxlength=array2.length 
-        
-        for i in 0...maxlength
-          array3[i]=array1[i] + array2[array2.length-1-i]
-        end   
-        array3
-    end
+# frozen_string_literal:true
+
+# Mereg array
+class MergeArray
+  def merge(array1, array2)
+    array3 = []
+    array1.delete('')
+    array2.delete('')
+    (0...maxlength(array1, array2)).each do |i| 
+      array3[i] = array1[i] + array2[array2.length - 1 - i] 
+    end  
+    array3
   end
-  array1=["a","b","c",""]
-  array2=["","d","e","","f","u"]
-  obj=MergeArray.new
-  print obj.merge(array1,array2)
-   
+
+  def maxlength(array1, array2)
+    [array1.length, array2.length].max
+  end  
+
+end
+obj = MergeArray.new
+print obj.merge(['a', 'b', 'c', ''], ['', 'd', 'e', '', 'f', 'u'])
